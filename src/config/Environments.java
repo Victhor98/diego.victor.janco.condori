231b8e0b7cc67dac30b20d2d7c3206bea76c4494
diegojanco <janco3513@gmail.com>
2023-12-05 17:21:46 -0400

    first-commit

---
+++ src/config/Environments.java
@@ -0,0 +1,7 @@
+package config;
+
+public class Environments {
+    public static String lag = "en";
+    public static String pathFile = "/progra2/tema4/app/data/user.txt";
+    public static String pathFileProducts = "/progra2/tema4/app/data/product.txt";
+}
